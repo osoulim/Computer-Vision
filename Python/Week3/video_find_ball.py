33d966be865943691f05a54963828a05ffcc8703
Mamzi <mohammadreza.osouli@gmail.com>
2019-05-23 14:33:42 +0430

    Add bounding rect

--- Python/Week3/video_find_ball.py
+++ Python/Week3/video_find_ball.py
@@ -20,24 +20,29 @@ while True:
     erosion = cv2.erode(binary, kernel, iterations=1)
     erosion = cv2.dilate(erosion, kernel, iterations=5)
     cnts, _ = cv2.findContours(erosion, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    for c in cnts:
-        M = cv2.moments(c)
-        cX = int(M["m10"] / M["m00"])
-        cY = int(M["m01"] / M["m00"])
-        epsilon = cv2.arcLength(c, closed=True) * 0.01
-        approx = cv2.approxPolyDP(c, epsilon, True)
-        if len(approx) == 4:
-            shape = "square"
-        elif len(approx) == 5:
-            shape = "pentagon"
-        else:
-            shape = "circle"
-        cv2.putText(img, shape, (20, 20),
-                    cv2.FONT_HERSHEY_COMPLEX, 0.8, (0, 255, 0), 3)
-        (x, y), radius = cv2.minEnclosingCircle(c)
-        cv2.circle(img, (int(x), int(y)), int(radius), (0, 255, 255), 3)
-        # cv2.drawContours(img, [approx], 0, (0, 255, 0), 3)
-        # print(cv2.contourArea(c), cv2.arcLength(c, closed=True))
+    c = max(cnts, key=cv2.contourArea)
+    M = cv2.moments(c)
+    cX = int(M["m10"] / M["m00"])
+    cY = int(M["m01"] / M["m00"])
+    epsilon = cv2.arcLength(c, closed=True) * 0.01
+    approx = cv2.approxPolyDP(c, epsilon, True)
+    if len(approx) == 4:
+        shape = "square"
+    elif len(approx) == 5:
+        shape = "pentagon"
+    else:
+        shape = "circle"
+    cv2.putText(img, shape, (20, 20),
+                cv2.FONT_HERSHEY_COMPLEX, 0.8, (0, 255, 0), 3)
+
+    # (x, y), radius = cv2.minEnclosingCircle(c)
+    # cv2.circle(img, (int(x), int(y)), int(radius), (0, 255, 255), 3)
+
+    x, y, w, h = cv2.boundingRect(c)
+    cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 255), 3)
+
+    # cv2.drawContours(img, [approx], 0, (0, 255, 0), 3)
+    # print(cv2.contourArea(c), cv2.arcLength(c, closed=True))
     cv2.imshow("myImage", img)
 
 cam.release()
