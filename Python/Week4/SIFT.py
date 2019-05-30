2787e7223135f3cbb349d6755820223a2d26019a
Mamzi <mohammadreza.osouli@gmail.com>
2019-05-30 15:43:13 +0430

    Add sift

---
+++ Python/Week4/SIFT.py
@@ -0,0 +1,27 @@
+import cv2
+import numpy as np
+import imutils
+
+book1 = cv2.imread("files/book1.jpg")
+book2 = cv2.imread("files/book2.JPG")
+
+book1 = imutils.resize(book1, width=500)
+book2 = imutils.resize(book2, width=700)
+# book2 = cv2.cvtColor(book2, cv2.COLOR_BGR2GRAY)
+
+sift = cv2.xfeatures2d.SIFT_create()
+kp1, desc1 = sift.detectAndCompute(book1, None)
+kp2, desc2 = sift.detectAndCompute(book2, None)
+
+# cv2.drawKeypoints(book1, kp1, book1,
+#                   flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+
+bf = cv2.BFMatcher(cv2.NORM_L2, crossCheck=True)
+
+match = bf.match(desc1, desc2)
+match = sorted(match, key=lambda x: x.distance)
+result = cv2.drawMatches(book1, kp1, book2, kp2, match[0:40], None)
+
+cv2.imshow("book1", result)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
