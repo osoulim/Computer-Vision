4e762162086ef69e142404fd2a96f93853c5be6f
Mamzi <mohammadreza.osouli@gmail.com>
2019-06-13 15:22:27 +0430

    Fix typo

--- Python/Week5/hog.py
+++ Python/Week5/hog.py
@@ -1,8 +1,10 @@
 import cv2
 from skimage import feature
 import imutils
+import numpy as np
 
-img = imutils.resize(cv2.imread("files/pic1.jpg"), width=500)
+img = cv2.imread("files/rasht.jpg")
+img = imutils.resize(img, width=900)
 
 H, hogImage = feature.hog(img, orientations=8, pixels_per_cell=(10, 10),
                           cells_per_block=(3, 3), block_norm="L1", transform_sqrt=True, visualize=True)
