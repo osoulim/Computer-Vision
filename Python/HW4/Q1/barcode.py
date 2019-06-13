7ab9840df60ecd843b29aa753762f105a3cc826c
Mamzi <mohammadreza.osouli@gmail.com>
2019-06-13 13:43:00 +0430

    Fix barcode

--- Python/HW4/Q1/barcode.py
+++ Python/HW4/Q1/barcode.py
@@ -1,31 +1,38 @@
 import cv2
 import numpy as np
+import sys
 
-img = cv2.imread("Files/barcode_04.jpg")
+file = sys.argv[1] if len(sys.argv) > 1 else "Files/barcode_1.jpg"
+
+
+img = cv2.imread(file)
 grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-sobelX = cv2.Sobel(grey, cv2.CV_64F, dx=1, dy=0, ksize=-3)
-sobelY = cv2.Sobel(grey, cv2.CV_64F, dx=0, dy=1, ksize=-3)
+sobelX = cv2.Sobel(grey, cv2.CV_64F, dx=1, dy=0, ksize=3)
+sobelY = cv2.Sobel(grey, cv2.CV_64F, dx=0, dy=1, ksize=3)
 
 edges = cv2.subtract(sobelX, sobelY)
 edges = cv2.convertScaleAbs(edges)
 
 edges = cv2.blur(edges, (9, 9))
-_, thresh = cv2.threshold(edges, 220, 255, cv2.THRESH_BINARY)
+_, thresh = cv2.threshold(edges, 200, 255, cv2.THRESH_BINARY)
 
-closed = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, np.ones((7, 21)))
+morph = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, np.ones((10, 30)))
+morph = cv2.erode(morph, np.ones((3, 3)))
+# morph = cv2.dilate(morph, np.ones((3, 3)))
 
 
 _, cnt, _ = cv2.findContours(
-    closed, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
+    morph, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 maxCountor = max(cnt, key=cv2.contourArea)
 
+maxRect = cv2.minAreaRect(maxCountor)
+box = cv2.boxPoints(maxRect).astype(int)
+cv2.drawContours(img, [box], -1, (0, 255, 255), 3)
+# x, y, w, h = cv2.boundingRect(maxCountor)
 
-x, y, w, h = cv2.boundingRect(maxCountor)
+# cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 255), 3)
 
-cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 255), 3)
-
-cv2.imshow("barcode", closed)
+cv2.imshow("barcode", img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
