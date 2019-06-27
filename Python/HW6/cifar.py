937c969cb01da45ab92744d79b0a82f4220d38ac
Mohammadreza Osouli <osoulim@yahoo.com>
2019-06-27 09:07:16 +0430

    Add Knn classifier

--- Python/HW6/cifar.py
+++ Python/HW6/cifar.py
@@ -3,6 +3,7 @@ import cv2
 import glob as gb
 from skimage.feature import hog
 from sklearn.svm import LinearSVC
+from sklearn.neighbors import KNeighborsClassifier
 
 
 img_shape = (32, 32)
@@ -15,7 +16,7 @@ def unpickle(file):
         dict = pickle.load(fo, encoding='bytes')
     return dict
 
-def extract_cifar_batch(batch_name):
+def extract_cifar_batch(file):
     dic = unpickle(file)
     data, labels = dic[b'data'], dic[b'labels']
     images = []
@@ -24,11 +25,23 @@ def extract_cifar_batch(batch_name):
         R, G, B = img_data[0: img_size], img_data[img_size: 2*img_size], img_data[2*img_size: ]
         temp[..., 0], temp[..., 1], temp[..., 2] = B, G, R
         images.append(temp.reshape((32, 32, 3)))
-    return np.array(images), np.array(labels)
+    return data, labels
 
 batchs = gb.glob("files/cifar10/data_batch_*")
-print(batchs)
+tests = gb.glob("files/cifar10/test_batch*")
 
+trainX, trainY = [], []
 for file in batchs:
     data, labels = extract_cifar_batch(file)
-    print(data.shape, labels.shape)
+    for index in range(len(data)):
+        trainX.append(data[index])
+        trainY.append(labels[index])
+        
+trainX, trainY = map(np.array, (trainX, trainY))
+testX, testY = map(np.array, extract_cifar_batch(tests[0]))
+
+print(trainX.shape, trainY.shape, testX.shape, testY.shape)
+
+KnnClassifier = KNeighborsClassifier()
+KnnClassifier.fit(trainX, trainY)
+print(KnnClassifier.score(testX, testY))
